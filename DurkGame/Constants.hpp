a45704ef2ff61a6f40741347ced2d734825a3454
durk <martinez.d048432@stu.sanjac.edu>
2023-11-16 15:36:58 -0600

    added some constants

---
+++ DurkGame/Constants.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <SDL.h>
+
+typedef SDL_Scancode DK_KEYS;
+
+const SDL_Scancode DKK_w = SDL_SCANCODE_W;
+const SDL_Scancode DKK_a = SDL_SCANCODE_A;
+const SDL_Scancode DKK_s = SDL_SCANCODE_S;
+const SDL_Scancode DKK_d = SDL_SCANCODE_D;
+const SDL_Scancode DKK_q = SDL_SCANCODE_Q;
+const SDL_Scancode DKK_e = SDL_SCANCODE_E;
+const SDL_Scancode DKK_r = SDL_SCANCODE_R;
+const SDL_Scancode DKK_f = SDL_SCANCODE_F;
+const SDL_Scancode DKK_t = SDL_SCANCODE_T;
+const SDL_Scancode DKK_g = SDL_SCANCODE_G;
+const SDL_Scancode DKK_y = SDL_SCANCODE_Y;
+const SDL_Scancode DKK_h = SDL_SCANCODE_H;
+const SDL_Scancode DKK_u = SDL_SCANCODE_U;
+const SDL_Scancode DKK_j = SDL_SCANCODE_J;
+const SDL_Scancode DKK_i = SDL_SCANCODE_I;
+const SDL_Scancode DKK_k = SDL_SCANCODE_K;
+const SDL_Scancode DKK_o = SDL_SCANCODE_O;
+const SDL_Scancode DKK_l = SDL_SCANCODE_L;
+const SDL_Scancode DKK_p = SDL_SCANCODE_P;
+const SDL_Scancode DKK_z = SDL_SCANCODE_Z;
+const SDL_Scancode DKK_x = SDL_SCANCODE_X;
+const SDL_Scancode DKK_c = SDL_SCANCODE_C;
+const SDL_Scancode DKK_v = SDL_SCANCODE_V;
+const SDL_Scancode DKK_b = SDL_SCANCODE_B;
+const SDL_Scancode DKK_n = SDL_SCANCODE_N;
+const SDL_Scancode DKK_m = SDL_SCANCODE_M;
+const SDL_Scancode DKK_0 = SDL_SCANCODE_0;
+const SDL_Scancode DKK_1 = SDL_SCANCODE_1;
+const SDL_Scancode DKK_2 = SDL_SCANCODE_2;
+const SDL_Scancode DKK_3 = SDL_SCANCODE_3;
+const SDL_Scancode DKK_4 = SDL_SCANCODE_4;
+const SDL_Scancode DKK_5 = SDL_SCANCODE_5;
+const SDL_Scancode DKK_6 = SDL_SCANCODE_6;
+const SDL_Scancode DKK_7 = SDL_SCANCODE_7;
+const SDL_Scancode DKK_8 = SDL_SCANCODE_8;
+const SDL_Scancode DKK_9 = SDL_SCANCODE_9;
+const SDL_Scancode DKK_left = SDL_SCANCODE_LEFT;
+const SDL_Scancode DKK_right = SDL_SCANCODE_RIGHT;
+const SDL_Scancode DKK_down = SDL_SCANCODE_DOWN;
+const SDL_Scancode DKK_up = SDL_SCANCODE_UP;
+const SDL_Scancode DKK_space = SDL_SCANCODE_SPACE;
+const SDL_Scancode DKK_return = SDL_SCANCODE_RETURN;
+const SDL_Scancode DKK_backspace = SDL_SCANCODE_BACKSPACE;
+const SDL_Scancode DKK_tab = SDL_SCANCODE_TAB;
+const SDL_Scancode DKK_lshift = SDL_SCANCODE_LSHIFT;
+const SDL_Scancode DKK_rshift = SDL_SCANCODE_RSHIFT;
+const SDL_Scancode DKK_lctrl = SDL_SCANCODE_LCTRL;
+const SDL_Scancode DKK_rctrl = SDL_SCANCODE_RCTRL;
+const SDL_Scancode DKK_lalt = SDL_SCANCODE_LALT;
+const SDL_Scancode DKK_ralt = SDL_SCANCODE_RALT;
+
+const int DK_KEYDOWN = SDL_KEYDOWN;
+const int DK_KEYUP = SDL_KEYUP;
+const int DK_QUIT = SDL_QUIT;
+const int DKK_ESCAPE = SDLK_ESCAPE;
+const int DK_MOUSEBUTTONDOWN = SDL_MOUSEBUTTONDOWN;
+const int DK_MOUSEBUTTONUP = SDL_MOUSEBUTTONUP;
+const int DK_MOUSEMOTION = SDL_MOUSEMOTION;
+const int DK_MOUSEWHEEL = SDL_MOUSEWHEEL;
+
