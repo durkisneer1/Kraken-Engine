8e8abf494a905a3a2a9f096cda693a27af08228a
durk <martinez.d048432@stu.sanjac.edu>
2024-01-16 04:41:20 -0600

    removed warning in docs

--- include/Mixer.hpp
+++ include/Mixer.hpp
@@ -34,8 +34,6 @@ class Sound final
      * @param loops The number of times to loop the sound.
      * @param playTime The number of milliseconds to play the sound for.
      * @param fadeMs The number of milliseconds to fade in.
-     *
-     * @warning ``fadeMS`` is currently not working.
      */
     void play(int loops = 0, int playTime = -1, int fadeMs = 0);
 
