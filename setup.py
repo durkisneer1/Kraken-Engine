0df021658d97391f3b71339a2fd1de9b4eb9875a
Johan Mallqui <johanmmalqui@gmail.com>
2024-09-01 23:39:57 +0900

    Move setup.py to root directory level (3)

--- setup.py
+++ setup.py
@@ -23,29 +23,3 @@ def meson_build():
 
 
 subprocess.run(meson_build(), shell=True)
-
-import subprocess
-import os
-import sys
-
-if not os.path.exists("subprojects"):
-    os.mkdir("subprojects")
-
-    wraps = [
-        "sdl2",
-        "sdl2_image",
-        "sdl2_ttf",
-        "sdl2_mixer",
-    ]
-    for wrap in wraps:
-        print(f"Installing wrap: {wrap}")
-        subprocess.run(f"meson wrap install {wrap}", shell=True)
-
-
-def meson_build():
-    command = "meson setup".split(" ")
-    command.extend(sys.argv[1:])
-    return " ".join(command)
-
-
-subprocess.run(meson_build(), shell=True)
