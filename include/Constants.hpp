410ea24f43833b8b0d731b96aa623f2afadb2b23
JohanMallqui <106215428+jmmalqui@users.noreply.github.com>
2024-09-05 01:17:28 +0000

    Math module new methods. (#17)
    * Update installation.rst
    
    * Meson SDL dependencies fix
    
    * Move setup.py to root directory level
    
    * Move setup.py to root directory level (3)
    
    * Move setup.py to root directory level (4)
    
    * Math module methods.
    
    Added various methods for the math module.

--- include/Constants.hpp
+++ include/Constants.hpp
@@ -1,7 +1,5 @@
 #pragma once
-
 #include <SDL.h>
-
 namespace kn
 {
 typedef SDL_Scancode KEYS;
--- include/Math.hpp
+++ include/Math.hpp
@@ -11,6 +11,17 @@ using overflow::isSumValid;
 
 namespace math
 {
+
+/**
+ * @brief Polar Coordinate class
+ */
+
+struct PolarCoordinate
+{
+    double angle;
+    double radius;
+};
+
 /**
  * @brief A 2D vector.
  */
@@ -48,11 +59,82 @@ class Vec2
     double getLength() const;
 
     /**
+     * @brief Calculates the angle in radians between two vectors.
+     * @return The angle in radians between two vectors.
+     */
+    double angleTo(const Vec2& other) const;
+
+    /**
+     * @brief Get the dot product of two vectors.
+     * @return The dot product of two vectors.
+     */
+    double dot(const Vec2& other) const;
+    /**
+     * @brief Get the cross product of two vectors.
+     * @return The cross product of two vectors.
+     */
+    double cross(const Vec2& ohter) const;
+    /**
+     * @brief Rotates a vector in-place by a given angle in degrees
+     *
+     */
+    void rotate(double angle);
+    /**
+     * @brief Rotates a vector in-place by a given angle in radians
+     *
+     */
+    void rotateRad(double angle);
+
+    /**
+     * @brief Converts a vector into it's polar representation
+     * @return Returns a PolarCoordinate struct which contains an angle in degrees and the vector
+     * magnitude as radius.
+     */
+    PolarCoordinate asPolar();
+    /**
+     * @brief Constructs a vector from it's polar representation.
+     * @return Returns a vector from it's polar representation.
+     */
+    Vec2 fromPolar(double angle, double radius);
+    /**
+     * @brief Scales a vector to a given value in-place.
+     *
+     */
+    void scaleToLengthIP(double value);
+    /**
+     * @brief Scales a vector to a given value and returns it.
+     * @return Returns a vector of magnitude equal to a given value while mantaining it's original
+     * direction.
+     *
+     */
+    Vec2 scaleToLength(double value);
+    /**
+     * @brief Calculates the vector projection of this vector onto another vector.
+     * @return Returns the projection of a vector onto another vector.
+     */
+    Vec2 project(const Vec2& other);
+    /**
+     * @brief Calculates the vector rejection of this vector onto another vector.
+     * @return Returns the vector rejection between two vectors.
+     */
+    Vec2 reject(const Vec2& other);
+    /**
+     * @brief Calculates the reflection of a vector onto another vector.
+     * @return  Returns the reflection of a vector onto another vector.
+     */
+    Vec2 reflect(const Vec2& other) const;
+    /**
      * @brief Normalize the vector in-place. Fails if an overflow occurs or the vector is the zero
      * vector
      */
-    [[maybe_unused]] bool normalize();
+    [[maybe_unused]] bool normalizeIP();
 
+    /**
+     * @brief Normalize the vector and returns it. Fails if an overflow occurs or the vector is the
+     * zero vector.
+     * @return A normalized vector.
+     */
+    [[maybe_unused]] Vec2 normalize() const;
     /**
      * @brief Get the distance to another vector.
      *
@@ -129,6 +211,26 @@ Vec2 clampVec(const Vec2& vec, const Vec2& min, const Vec2& max);
  * @return The interpolated vector.
  */
 Vec2 lerpVec(const Vec2& a, const Vec2& b, double t);
+
+/**
+ * @brief  Remaps a value from an input range to an output range
+ * @return Returns a value that was mapped from [in_min, in_max] to [out_min, out_max], if in_min
+ * and in_max are the same then it returns 0 and raises a WARN message.
+ */
+
+double remap(double in_min, double in_max, double out_min, double out_max, double value);
+
+/**
+ * @brief Converts an angle to degrees.
+ * @return Returns the representation of an angle in degrees.
+ */
+double toDegrees(double angle);
+/**
+ * @brief Converts an angle to radians.
+ * @return Returns the representation of an angle in radians.
+ *
+ */
+double toRadians(double angle);
 
 /**
  * @brief Multiplies a vector by a constant
